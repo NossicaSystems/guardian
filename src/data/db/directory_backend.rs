4b2b91f7a82b2c03a5185a03352da611207cc81d
Nossica <nossicasystems@gmail.com>
2025-04-30 22:50:09 +0100

    refined file paths

--- src/data/db/directory_backend.rs
+++ src/data/db/directory_backend.rs
@@ -1,19 +1,20 @@
-    use crate::data::db::StorageBackend;
+use crate::data::db::StorageBackend;
 
-    pub struct DirectoryBackend {
-        pub path: String,
+pub struct DirectoryBackend {
+    pub path: String,
+}
+
+impl StorageBackend for DirectoryBackend {
+    fn save_file(&self, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let full_path = format!("{}/{}", self.path, file_name);
+        println!("full path {}", full_path);
+        //std::fs::write(full_path, contents)?;
+        Ok(())
     }
 
-    impl StorageBackend for DirectoryBackend {
-        fn save_file(&self, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
-            let full_path = format!("{}/{}", self.path, file_name);
-            //std::fs::write(full_path, contents)?;
-            Ok(())
-        }
-
-        fn load_file(&self, file_name: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-            let full_path = format!("{}/{}", self.path, file_name);
-            let contents = std::fs::read(full_path)?;
-            Ok(contents)
-        }
+    fn load_file(&self, file_name: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let full_path = format!("{}/{}", self.path, file_name);
+        let contents = std::fs::read(full_path)?;
+        Ok(contents)
     }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod data;
 mod config;
+mod data;
 
-use std::fs;
 use clap::{Parser, Subcommand};
-use data::db::{FileEntry, DataStore};
 use config::Config;
+use data::db::{DataStore, FileEntry};
+use std::fs;
 
 #[derive(Parser)]
 #[command(name = "guardian")]
@@ -16,28 +16,25 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Store a file
-    S {
-        filename: String,
-    },
+    S { filename: String },
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   // let cli = Cli::parse();
-    //let config = Config::from_file("C:\\Users\\steev\\RustroverProjects\\guardian\\src\\config.yaml")?;
-    let contents = fs::read_to_string("C:\\Users\\steev\\RustroverProjects\\guardian\\src\\config.yaml")?;
+    println!("working directory is {:?}", std::env::current_dir());
+    let contents = fs::read_to_string("config.yaml")?;
     let config: Config = serde_yaml::from_str(&contents)?;
     println!("Loaded config: {:?}", config);
-    let mut data_store = data::db::DataStore{backend: None };
+    let mut data_store = data::db::DataStore { backend: None };
     data_store.load_config(config);
-    data_store.save_file("C:\\Users\\steev\\RustroverProjects\\guardian\\src\\test.txt");
+    data_store.save_file("test.txt");
 
-/*    match cli.command {
-        Commands::S { filename } => {
-            let file = FileEntry::from_path(&filename)?;
-            data_store.save_file(&file);
-            println!("File saved.");
+    /*    match cli.command {
+            Commands::S { filename } => {
+                let file = FileEntry::from_path(&filename)?;
+                data_store.save_file(&file);
+                println!("File saved.");
+            }
         }
-    }
-*/
+    */
     Ok(())
 }
